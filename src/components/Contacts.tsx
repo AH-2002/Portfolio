b4794ca9b539cc4f0d4f410cff73bd921b55422f
AH-2002 <xabdo608@gmail.com>
2025-10-07 19:03:59 +0300

    update navbar and contacts section

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ContactForm from './ContactForm'
 import { PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/outline'
 import { personalInfo } from '@/constants/PersonalInfo'
+import SectionHeading from './SectionHeader'
 
 export default function Contacts() {
   return (
     <section className="py-16 bg-[#050709]" id="contacts">
+      <SectionHeading>Contacts</SectionHeading>
       <div className="grid grid-cols-1 xl:grid-cols-2 w-[90%] sm:w-[80%] mx-auto items-center gap-10 mt-10 ">
         <div
           data-aos="fade-left"
           data-aos-anchor-placement="top-center"
+        className="mt-10"
         >
           <ContactForm />
         </div>
--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -3,7 +3,8 @@ import { NavLink } from "@/types/navType";
 export const navLinks: NavLink[] = [
     { id: 1, label: "Home", href: "#hero" },
     { id: 2, label: "About", href: "#about" },
-    { id: 3, label: "Projects", href: "#projects" },
-    { id: 4, label: "Skills", href: "#skills" },
-    { id: 5, label: "Contacts", href: "#contacts" },
+    { id: 3, label: "Experience", href: "#experience" },
+    { id: 4, label: "Projects", href: "#projects" },
+    { id: 5, label: "Skills", href: "#skills" },
+    { id: 6, label: "Contacts", href: "#contacts" },
 ]
