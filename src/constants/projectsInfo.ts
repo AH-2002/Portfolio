38ccae5b95fcd4fcd493ec2722a40b4c92965f08
AH-2002 <xabdo608@example.com>
2025-12-07 02:10:24 +0200

    add neww project

--- src/constants/projectsInfo.ts
+++ src/constants/projectsInfo.ts
@@ -1,34 +1,44 @@
 import { projectsType } from "@/types/projectsType";
 export const projects: projectsType[] = [
-    {
-        title: "Movies Website",
-        description: "A React SPA for browsing trending movies and TV shows with pagination and JWT auth.",
-        github: "https://github.com/AH-2002/movies-app",
-        live: "https://noxe-gamma.vercel.app/#/login",
-        image: "/images/movies4.png",
-    },
-    {
-        title: "E-Commerce Platform",
-        description: "An interactive e-commerce store built with React, Context API, and Axios for dynamic product loading.",
-        github: "https://github.com/AH-2002/e-commerce",
-        live: "https://e-commerce-tau-seven-53.vercel.app/",
-        image: "/images/e-commerce.png",
+  {
+    title: "Movies Website",
+    description:
+      "A React SPA for browsing trending movies and TV shows with pagination and JWT auth.",
+    github: "https://github.com/AH-2002/movies-app",
+    live: "https://noxe-gamma.vercel.app/#/login",
+    image: "/images/movies4.png",
+  },
+  {
+    title: "E-Commerce Platform",
+    description:
+      "An interactive e-commerce store built with React, Context API, and Axios for dynamic product loading.",
+    github: "https://github.com/AH-2002/e-commerce",
+    live: "https://e-commerce-tau-seven-53.vercel.app/",
+    image: "/images/e-commerce.png",
+  },
+  {
+    title: "React.js Social Media App (Ze)",
+    description:
+      "A social platform using React, Redux Toolkit, and DummyJSON API with full authentication.",
+    github: "https://github.com/AH-2002/Ze",
+    live: "https://ze-drab.vercel.app/",
+    image: "/images/zee.png",
+  },
+  {
+    title: "Admin Dashboard",
+    description:
+      "Admin Dashboard built with Next.js 15, featuring Clerk authentication, dynamic routing, and responsive UI components. It includes pages for customers, invoices, profile management, and secure auth flows (login/signup). Styled with Tailwind CSS and optimized for production.",
+    github: "https://github.com/AH-2002/Admin-Dashboard",
+    live: "https://admin-dashboard-seven-kappa-49.vercel.app/",
 
-    },
-    {
-        title: "Social Media App",
-        description: "A social platform using React, Redux Toolkit, and DummyJSON API with full authentication.",
-        github: "https://github.com/AH-2002/Ze",
-        live: "https://ze-drab.vercel.app/",
-        image: "/images/zee.png",
-
-    },
-    {
-        title: "Admin Dashboard",
-        description: "Admin Dashboard built with Next.js 15, featuring Clerk authentication, dynamic routing, and responsive UI components. It includes pages for customers, invoices, profile management, and secure auth flows (login/signup). Styled with Tailwind CSS and optimized for production.",
-        github: "https://github.com/AH-2002/Admin-Dashboard",
-        live: "https://admin-dashboard-seven-kappa-49.vercel.app/",
-        
-        image: "/images/dashboard.png",
-    },
+    image: "/images/dashboard.png",
+  },
+  {
+    title: "Next.js Social Media App (Voxa)",
+    description:
+      "Voxa is a social media app for creating posts, liking, commenting, and keeping personal diaries. Built with Next.js, TypeScript, Tailwind, and shadcn/ui, it uses JWT authentication for security and MongoDB for fast, reliable data storage.",
+    github: "https://github.com/AH-2002/VOXA",
+    live: "https://voxa-lemon.vercel.app/",
+    image: "/images/voxa.png",
+  },
 ];
